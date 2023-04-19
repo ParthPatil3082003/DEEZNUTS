2016ba2d120beadd6008ea234f12cff83d82544a
Mayuresh1611 <mayur1611personal@gmail.com>
2023-04-19 17:01:47 +0530

    Did changes to the visibility aspects

--- client/Source.cpp
+++ client/Source.cpp
@@ -10,7 +10,7 @@
      .
      .
      -----***** WARNING FOR SRVER *****---------------
-     while recieving these signal bytes 
+     while recieving these signal bytes
      do not convert recieved bytes to int instead compare them with predefined byte like
      char online = 0x00
      if ( recv_byte == online)
@@ -30,20 +30,21 @@
 #define hostname "deeznutserver.centralindia.cloudapp.azure.com"
 
 // initialized neccessary variables 
-int loop = 1 , key , bytes_read;
-char msgsend[500] , recvbyte;
-char msgrecv[500];
-std::string msg , username;
+int loop = 1;
+int key, bytes_read;
+char msgsend[500], recvbyte;
+char msgrecv[512];
+std::string msg, username;
 
 // function to send signal to server in byte form 
 // takes scoket and required byte( i.e 0x01 etc. ) as input 
-void signal( SOCKET sock , char bt) {
+void signal(SOCKET sock, char bt) {
     send(sock, &bt, 1, 0);
 }
 
 // sends text message to the server
 // also returns the message sent in string 
-std :: string sendtoserver(SOCKET sock) {
+std::string sendtoserver(SOCKET sock) {
     std::getline(std::cin, msg);
     strncpy_s(msgsend, msg.c_str(), sizeof(msgsend));
     send(sock, msgsend, strlen(msgsend), 0);
@@ -54,41 +55,42 @@ std :: string sendtoserver(SOCKET sock) {
 // press '/' to chat and 'esc' to disconnect
 // runs in thread
 void sendmsg(SOCKET sock) {
-
     // loop keeps running until true and stops when condition changed to 0
     while (loop) {
+
         // waits for the character
-        key = _getch();
+        key = getchar();
 
         // if 'esc' pressed it sets 
         if (key == 27) {
             loop = 0;           //loop = 0 so that while loop of both send and recv function stops
+
             signal(sock, 0x00); // signals the server that client is offline
             break;              // breaks out of loop and finishes the program
         }
         // if '/' pressed calls the sendtoserver function to send the text message
         else if (key == 47) {
-            printf("YOU: ");
+            signal(sock, 0x05);
             sendtoserver(sock);
         }
+
     }
 }
 
 // this function recievs the chat
 // runs in thread
 // keeps posting incoming messages
-void recvmsg(SOCKET sock , std::string username ) {
+void recvmsg(SOCKET sock, std::string username) {
     while (loop) {
-        bytes_read = recv(sock, msgrecv, 500, 0);
-        msgrecv[bytes_read] = '\0';
-        printf(username.c_str(), ":", msgrecv, "\n");
+        bytes_read = recv(sock, msgrecv, 512, 0);
+        printf("%s: %s\n", username.c_str(), msgrecv);
     }
 }
 
 
 int main() {
-	
-	// initializing the socket
+
+    // initializing the socket
     WSAData wsaData;
     int wsaerr = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (wsaerr != 0) {
@@ -104,7 +106,7 @@ int main() {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    int getret = getaddrinfo( hostname , "42069", &hints, &result);
+    int getret = getaddrinfo(hostname, "42069", &hints, &result);
     if (getret != 0) {
         printf("getaddrinfo failed\n");
         WSACleanup();
@@ -138,17 +140,18 @@ int main() {
     signal(clientsock, 0x02); // signals server that new user is joining and to tells to get ready to revieve username
     sendtoserver(clientsock); // sends username to the server
 
-    signal(clientsock, 0x01); // signals that user is online
+    //    signal(clientsock, 0x01); // signals that user is online
 
-    // takes username of the client to whome to chat with
+        // takes username of the client to whome to chat with
     do {
         printf("Enter username to connect\n>>> "); // enter username of the client
+        signal(clientsock, 0x06);
         username = sendtoserver(clientsock); // sends client username to server to verify
         recv(clientsock, &recvbyte, 1, 0); // server sends verification signal 
-        
+
         // 0x03 for client username is available 0x04 for not
         if ((int)recvbyte == 0x03) {
-            printf("connecting to ", username.c_str(), "\n");
+            printf("connecting to %s", username.c_str(), "\n");
             break;
         }
         else {
@@ -158,16 +161,17 @@ int main() {
     } while (1);
 
     // runs the thread to send and recieve funtions to immitate chat
-    std::thread recievechat(recvmsg , clientsock, username);
+
+    std::thread recievechat(recvmsg, clientsock, username);
     std::thread sendchat(sendmsg, clientsock);
-    
+
     // joins thread to main thread
+
     sendchat.join();
     recievechat.join();
 
     // fuking done
     closesocket(clientsock);
     WSACleanup();
-	return 0;
+    return 0;
 }
-
